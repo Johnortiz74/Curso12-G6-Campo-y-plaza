9a6751aabdcaecfa52c83f28eb3d866cd25818ca
luismolinamt <luis.molina.mt@usa.edu.co>
2022-12-07 09:55:53 -0500

    septimo commit back

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const conectarDB = async () => {
     try{
         const connection = await mongoose.connect(
-            "mongodb+srv://jhonny:Sergio2022*@cluster0.sbvhstb.mongodb.net/GraphQL",{
+            "mongodb+srv://Fede:moli@cluster0.adxwxgl.mongodb.net/?retryWrites=true&w=majority",{
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             });
