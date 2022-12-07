7aef1ffddec279188f64fe5fbbaa2d50541b45ae
luismolinamt <luis.molina.mt@usa.edu.co>
2022-12-07 10:02:33 -0500

    septimo commit front

--- src/componentes/Admin.js
+++ src/componentes/Admin.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const Admin = () => {
   
@@ -18,34 +20,21 @@ const Admin = () => {
   },[navigate]);//[] se ejecuta solo una vez
   
   
-  
-  
-  
-  const cerrarSesion = () =>{
-    localStorage.removeItem("token");
-    navigate("/");
-  }
+ 
 
 
   return (
-   <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
-   <div className='md:w-2/3 lg:w-2/5'>
-   <h1 className="inline bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-      Panel de Administrador 4
+    <>
+      <Header/>
+      <div className="md:flex md:min-h-screen">
+        <Sidebar/>
+        <h1 className="inline bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+      lista de categorias
     </h1>
-
-    <input
-      type='submit'
-      value="Cerrar Sesión"
-      className="bg-violet-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-violet-300 transition-colors"
-      onClick={cerrarSesion}
-    />
-   
-
-
-   </div>
+      </div>
     
-   </main>
+    
+    </>
     );
 }
 
